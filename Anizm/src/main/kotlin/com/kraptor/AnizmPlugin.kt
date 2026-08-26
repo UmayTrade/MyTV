ead9ad4355e3620c00bf8c8a5d2f350835d404d4
UmayTrade <kocatepe828@gmail.com>
2026-08-26 16:07:12 +0300

    Add files via upload

---
+++ Anizm/src/main/kotlin/com/kraptor/AnizmPlugin.kt
@@ -0,0 +1,16 @@
+// ! Bu araç @kraptor123 tarafından yazılmıştır.
+
+package com.kraptor
+
+import android.content.Context
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.Plugin
+
+@CloudstreamPlugin
+class AnizmPlugin: Plugin() {
+    override fun load(context: Context) {
+        registerMainAPI(Anizm())
+        registerExtractorAPI(AincradExtractor())
+        registerExtractorAPI(GoogleDriveExtractor())
+    }
+}
