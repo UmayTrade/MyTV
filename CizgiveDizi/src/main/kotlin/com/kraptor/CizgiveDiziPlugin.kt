2ae828e4ad743eb1582730cc8b20794c7c3a7928
UmayTrade <kocatepe828@gmail.com>
2026-08-26 16:12:43 +0300

    Add files via upload

---
+++ CizgiveDizi/src/main/kotlin/com/kraptor/CizgiveDiziPlugin.kt
@@ -0,0 +1,16 @@
+package com.kraptor
+
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.Plugin
+import android.content.Context
+
+@CloudstreamPlugin
+class CizgiveDiziPlugin: Plugin() {
+    override fun load(context: Context) {
+        registerMainAPI(CizgiveDizi())
+        registerExtractorAPI(GoogleDriveExtractor())
+        registerExtractorAPI(SibNet())
+        registerExtractorAPI(CizgiDuo())
+        registerExtractorAPI(CizgiPass())
+    }
+}
