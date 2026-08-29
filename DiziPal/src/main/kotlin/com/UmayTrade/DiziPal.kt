d0ac6f9145cf1e5649e57b21ad5a1728a583434d
UmayTrade <kocatepe828@gmail.com>
2026-08-29 09:34:03 +0300

    Update DiziPal.kt

--- DiziPal/src/main/kotlin/com/UmayTrade/DiziPal.kt
+++ DiziPal/src/main/kotlin/com/UmayTrade/DiziPal.kt
@@ -9,7 +9,7 @@ import org.jsoup.nodes.Element
 
 class DiziPal : MainAPI() {
 
-    override var mainUrl = "https://dizipal1578.com"
+    override var mainUrl = "https://dizipal2121.com"
     override var name = "DiziPalim"
     override val hasMainPage = true
     override var lang = "tr"
