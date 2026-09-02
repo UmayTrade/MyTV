c328cfca25eb4aae79d3b371a18302c37b18a38d
UmayTrade <kocatepe828@gmail.com>
2026-09-02 15:44:32 +0300

    Update DiziPalModels.kt

--- DiziPal/src/main/kotlin/com/keyiflerolsun/DiziPalModels.kt
+++ DiziPal/src/main/kotlin/com/keyiflerolsun/DiziPalModels.kt
@@ -14,6 +14,6 @@ data class SearchItem(
     @JsonProperty("duration") val duration: String,
     @JsonProperty("year") val year: String,
     @JsonProperty("view") val view: Int,
-    @JsonProperty("type") val type: String = "defaultType",
+    @JsonProperty("type") val type: String = "series", // ! API'den gelmezse varsayılan dizi
     @JsonProperty("url") val url: String
 )
