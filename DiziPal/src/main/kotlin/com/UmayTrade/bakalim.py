9218d49381cfb5d1488e06afcbbcfa279a571aa8
UmayTrade <kocatepe828@gmail.com>
2026-09-03 03:53:34 +0300

    Update main URL for DiziPal scraper

--- DiziPal/src/main/kotlin/com/UmayTrade/bakalim.py
+++ DiziPal/src/main/kotlin/com/UmayTrade/bakalim.py
@@ -1,3 +1,4 @@
+# ! Bu araç @keyiflerolsun tarafından | @KekikAkademi için yazılmıştır.
 
 from Kekik.cli    import konsol
 from cloudscraper import CloudScraper
@@ -6,7 +7,7 @@ from re           import findall
 
 oturum  = CloudScraper()
 
-mainUrl = "https://dizipal1539.com"
+mainUrl = "https://dizipal737.com"
 pageUrl = f"{mainUrl}/diziler?kelime=&durum=&tur=1&type=&siralama="
 istek   = oturum.get(pageUrl)
 secici  = Selector(istek.text)
