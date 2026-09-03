683370044c242439e3b8947842451368e8935ca3
UmayTrade <kocatepe828@gmail.com>
2026-09-03 03:53:11 +0300

    Update DiziPalPlugin.kt

--- DiziPal/src/main/kotlin/com/UmayTrade/DiziPalPlugin.kt
+++ DiziPal/src/main/kotlin/com/UmayTrade/DiziPalPlugin.kt
@@ -1,4 +1,4 @@
-package com.keyiflerolsun
+package com.UmayTrade
 
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
@@ -7,6 +7,6 @@ import android.content.Context
 @CloudstreamPlugin
 class DiziPalPlugin: Plugin() {
     override fun load(context: Context) {
-        registerMainAPI(DiziPal())
+        registerMainAPI(DiziPalOriginal())
     }
 }
