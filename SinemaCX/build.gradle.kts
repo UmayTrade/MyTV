eefbf197f1d0a6ce184f4fca2fd24d75ab656016
UmayTrade <kocatepe828@gmail.com>
2026-09-04 00:59:22 +0300

    Add files via upload

--- SinemaCX/build.gradle.kts
+++ SinemaCX/build.gradle.kts
@@ -1,7 +1,7 @@
 version = 24
 
 cloudstream {
-    authors     = listOf("UmayTrade")
+    authors     = listOf("keyiflerolsun")
     language    = "tr"
     description = "Güncel ve en iyi yabancı filmleri yüksek görüntü kalitesinde, Türkçe dublaj ve altyazı seçenekleriyle filmleri full izleyin."
 
