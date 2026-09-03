02b8e18e93235cb80019cb081cbbb277dca4a801
UmayTrade <kocatepe828@gmail.com>
2026-09-04 01:35:58 +0300

    Update Sinezy.kt

--- Sinezy/src/main/kotlin/com/kraptor/Sinezy.kt
+++ Sinezy/src/main/kotlin/com/kraptor/Sinezy.kt
@@ -10,7 +10,7 @@ import com.lagradost.cloudstream3.LoadResponse.Companion.addActors
 import com.lagradost.cloudstream3.LoadResponse.Companion.addTrailer
 
 class Sinezy : MainAPI() {
-    override var mainUrl              = "https://sinezy.org"
+    override var mainUrl              = "https://sinezy.to/"
     override var name                 = "Sinezy"
     override val hasMainPage          = true
     override var lang                 = "tr"
