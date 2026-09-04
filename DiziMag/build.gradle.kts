affaab4b78081e00006758ee071b8d13f6300123
UmayTrade <kocatepe828@gmail.com>
2026-09-04 17:11:53 +0300

    Update build.gradle.kts

--- DiziMag/build.gradle.kts
+++ DiziMag/build.gradle.kts
@@ -1,17 +1,10 @@
 version = 18
 
 cloudstream {
-    authors     = listOf("UmayTrade")
+    authors     = listOf("nikyokki")
     language    = "tr"
     description = "En son çıkan yabancı dizi ve filmleri DiziMag farkıyla full hd 1080p kalitede izle. Dizimag Geniş ve güncel arşiviyle dizi ve filmlerin tadını çıkartın."
 
-    /**
-     * Status int as the following:
-     * 0: Down
-     * 1: Ok
-     * 2: Slow
-     * 3: Beta only
-     **/
     status  = 1
     tvTypes = listOf("TvSeries")
     iconUrl = "https://www.google.com/s2/favicons?domain=dizimag.net&sz=%size%"
