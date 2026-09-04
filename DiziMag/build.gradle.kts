745800e316b265a35dcd0031e30824bb2b798439
UmayTrade <kocatepe828@gmail.com>
2026-09-04 13:09:15 +0300

    Update build.gradle.kts

--- DiziMag/build.gradle.kts
+++ DiziMag/build.gradle.kts
@@ -14,5 +14,5 @@ cloudstream {
      **/
     status  = 1 // will be 3 if unspecified
     tvTypes = listOf("TvSeries")
-    iconUrl = "https://t2.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=https://dizimag.pro&size=32"
+    iconUrl = "https://www.google.com/s2/favicons?domain=dizimag.net&sz=%size%"
 }
