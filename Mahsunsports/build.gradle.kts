43064ea84bd603770faff4b7d58ec72cd8c97c93
UmayTrade <kocatepe828@gmail.com>
2026-09-05 03:24:05 +0300

    Add files via upload

---
+++ Mahsunsports/build.gradle.kts
@@ -0,0 +1,18 @@
+version = 4
+
+cloudstream {
+    authors     = listOf("Wiojelt")
+    language    = "tr"
+    description = "MahsunSports ile Türkiye'deki tüm spor kanallarını canlı izleyin. beIN Sports, S Sport, Tivibu, tabii, Exxen ve daha fazlası."
+
+    /**
+     * Status int as the following:
+     * 0: Down
+     * 1: Ok
+     * 2: Slow
+     * 3: Beta only
+    **/
+    status  = 1
+    tvTypes = listOf("Live")
+    iconUrl = "https://raw.githubusercontent.com/tv-logo/tv-logos/main/countries/turkey/trt-spor-tr.png"
+}
