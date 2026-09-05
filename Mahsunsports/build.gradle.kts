b0b54f922e69aa134b099ece3935037b20127910
UmayTrade <kocatepe828@gmail.com>
2026-09-05 03:44:01 +0300

    Update build.gradle.kts

--- Mahsunsports/build.gradle.kts
+++ Mahsunsports/build.gradle.kts
@@ -1,1 +1,11 @@
+version = 4
 
+cloudstream {
+    authors     = listOf("UmayTrade")
+    language    = "tr"
+    description = "MahsunSports ile Türkiye'deki tüm spor kanallarını canlı izleyin."
+
+    status  = 1
+    tvTypes = listOf("Live")
+    iconUrl = "https://raw.githubusercontent.com/tv-logo/tv-logos/main/countries/turkey/trt-spor-tr.png"
+}
