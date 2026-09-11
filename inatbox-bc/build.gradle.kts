13621864ba1865548414b35e0d22135f05363406
UmayTrade <kocatepe828@gmail.com>
2026-09-11 18:12:55 +0300

    Add files via upload

---
+++ inatbox-bc/build.gradle.kts
@@ -0,0 +1,23 @@
+version = 14
+
+cloudstream {
+    authors     = listOf("Dr.Octagon")
+    language    = "tr"
+    description = "İnatBox BronzeCloud Edition - Canlı kanallar, spor ve diziler burada."
+
+    /**
+     * Status int as the following:
+     * 0: Down
+     * 1: Ok
+     * 2: Slow
+     * 3: Beta only
+    **/
+    status  = 1
+    tvTypes = listOf("Movie", "TvSeries", "Live")
+    iconUrl = "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEh3vCp6N1K4bECoYRQD-cisJF2_6V_Hk01ZhDmoPR2JuM8O5qr4MqrPO1munM9cRlleBBSK6odYhLtDBWv4E3vhPhynlmS5hVVtJZShHoGA5REQ8_3v8SIlccTEqzVQu2UJyNYQdJNrKIfWy66RQeT0D-CcmFCbHPz5023H6p2v5fv4NVloZ5Rqo_yGrIY/s320/iNat-Box-App.png"
+}
+
+tasks.named<com.lagradost.cloudstream3.gradle.tasks.GenerateManifestTask>("generateManifest") {
+    pluginName.set("İnatBox BC")
+}
+
