65b5a2c4b92eaee52b929b22b4f0fdbe5f506ca2
UmayTrade <kocatepe828@gmail.com>
2026-09-11 12:56:07 +0300

    Update KanalDPlugin.kt

--- KanalD/src/main/kotlin/com/kerimmkirac/KanalDPlugin.kt
+++ KanalD/src/main/kotlin/com/kerimmkirac/KanalDPlugin.kt
@@ -5,8 +5,8 @@ import com.lagradost.cloudstream3.plugins.Plugin
 import android.content.Context
 
 @CloudstreamPlugin
-class AtvPlugin : Plugin() {
+class KanalDPlugin : Plugin() {
     override fun load(context: Context) {
-        registerMainAPI(Atv())
+        registerMainAPI(KanalD())
     }
 }
