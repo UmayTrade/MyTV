913adc7e17696fc921d12f116fd0aee88adb0ec4
UmayTrade <kocatepe828@gmail.com>
2026-09-14 18:50:41 +0300

    Update build.gradle.kts

--- YesilCamTv/build.gradle.kts
+++ YesilCamTv/build.gradle.kts
@@ -4,7 +4,7 @@ cloudstream {
     authors     = listOf("CloudStreamTR", "kraptor")
     language    = "tr"
     description = "Yeşilçam klasik Türk sineması filmleri ve arşiv"
-    status      = 1
+    status      = 0
     tvTypes     = listOf("Movie")
     iconUrl     = "https://www.google.com/s2/favicons?sz=128&domain=yesilcamtv.com.tr"
 }
