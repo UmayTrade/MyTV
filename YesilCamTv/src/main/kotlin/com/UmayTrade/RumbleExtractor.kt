7fe851471d82eabda74689e5fbdbfc32285e0a8d
UmayTrade <kocatepe828@gmail.com>
2026-09-15 18:29:22 +0300

    Update RumbleExtractor.kt

--- YesilCamTv/src/main/kotlin/com/UmayTrade/RumbleExtractor.kt
+++ YesilCamTv/src/main/kotlin/com/UmayTrade/RumbleExtractor.kt
@@ -1,16 +1,19 @@
 package com.UmayTrade
 
-import com.lagradost.cloudstream3.ExtractorLink
 import com.lagradost.cloudstream3.SubtitleFile
 import com.lagradost.cloudstream3.app
 import com.lagradost.cloudstream3.newSubtitleFile
 
 import com.lagradost.cloudstream3.utils.ExtractorApi
+import com.lagradost.cloudstream3.utils.ExtractorLink
 import com.lagradost.cloudstream3.utils.ExtractorLinkType
 import com.lagradost.cloudstream3.utils.Qualities
 import com.lagradost.cloudstream3.utils.newExtractorLink
 
+import org.json.JSONArray
 import org.json.JSONObject
+
+import java.net.URLDecoder
 
 class RumbleExtractor : ExtractorApi() {
 
