a1294ffe1baa97abd2225bca7fe76507a7da762b
UmayTrade <kocatepe828@gmail.com>
2026-09-20 00:51:50 +0300

    Update YesilCamTvPlugin.kt

--- YesilCamTv/src/main/kotlin/com/UmayTrade/YesilCamTvPlugin.kt
+++ YesilCamTv/src/main/kotlin/com/UmayTrade/YesilCamTvPlugin.kt
@@ -1,25 +1,11 @@
 package com.UmayTrade
 
+import com.lagradost.cloudstream3.plugins.BasePlugin
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
-import com.lagradost.cloudstream3.plugins.Plugin
 
 @CloudstreamPlugin
-class YesilCamTvPlugin : Plugin() {
-
-    override fun load(context: android.content.Context) {
-
-        /*
-         * Rumble özel extractor
-         */
-        registerExtractorAPI(
-            RumbleExtractor()
-        )
-
-        /*
-         * Ana site provider
-         */
-        registerMainAPI(
-            YesilCamTv()
-        )
+class YesilCamTvPlugin : BasePlugin() {
+    override fun load() {
+        registerMainAPI(YesilCamTv())
     }
 }
