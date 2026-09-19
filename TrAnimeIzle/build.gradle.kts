fbb1fc0fb95239e429e9ef876b5d81b74154dd49
UmayTrade <kocatepe828@gmail.com>
2026-09-19 03:42:10 +0300

    Update build.gradle.kts

--- TrAnimeIzle/build.gradle.kts
+++ TrAnimeIzle/build.gradle.kts
@@ -5,7 +5,7 @@ fun Project.cloudstream(configuration: CloudstreamExtension.() -> Unit) = extens
 version = 1
 
 cloudstream {
-    authors = listOf("ulgenzade")
+    authors = listOf("UmayTrade")
     language = "tr"
     description = "TrAnimeİzle - Türkçe Anime İzleme Platformu"
     status = 1
