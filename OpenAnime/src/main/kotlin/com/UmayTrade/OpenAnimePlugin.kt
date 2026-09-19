6eedc67426c1eebcd8ddddd67337376d85b3ca3b
UmayTrade <kocatepe828@gmail.com>
2026-09-19 03:49:23 +0300

    Update OpenAnimePlugin.kt

--- OpenAnime/src/main/kotlin/com/UmayTrade/OpenAnimePlugin.kt
+++ OpenAnime/src/main/kotlin/com/UmayTrade/OpenAnimePlugin.kt
@@ -4,6 +4,18 @@ import android.content.Context
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
 
+/**
+ * OpenAni.me Cloudstream Eklentisi
+ *
+ * Bu eklenti, OpenAni.me sitesindeki animeleri Cloudstream üzerinden
+ * izlenebilir hale getirir.
+ *
+ * Site: https://openani.me / https://openanime.org
+ * Yapı: Next.js (Pages Router) tabanlı SPA
+ * Oynatıcılar: Doğrudan HLS (m3u8), progressive MP4, harici embed
+ *              (Vidmoly, Sibnet, Doodstream vb.)
+ * DRM: Yok (clear HLS)
+ */
 @CloudstreamPlugin
 class OpenAnimePlugin : Plugin() {
     override fun load(context: Context) {
