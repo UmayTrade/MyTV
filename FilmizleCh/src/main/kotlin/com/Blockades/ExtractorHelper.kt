54f768d927495cf627ff9636ddf2d6ce20774fb3
UmayTrade <kocatepe828@gmail.com>
2026-09-19 18:06:35 +0300

    Create ExtractorHelper.kt

---
+++ FilmizleCh/src/main/kotlin/com/Blockades/ExtractorHelper.kt
@@ -0,0 +1,8 @@
+package com.Blockades
+
+fun getBrowserHeaders(referer: String): Map<String, String> {
+    return mapOf(
+        "User-Agent" to "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36",
+        "Referer" to referer
+    )
+}
