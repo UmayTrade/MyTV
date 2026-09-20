d1d2bcb132743e755c8dc34eda467fe5536dda90
UmayTrade <kocatepe828@gmail.com>
2026-09-21 00:51:43 +0300

    Update DramaDizilerimPlugin.kt

--- DramaDizilerim/src/main/kotlin/com/UmayTrade/DramaDizilerimPlugin.kt
+++ DramaDizilerim/src/main/kotlin/com/UmayTrade/DramaDizilerimPlugin.kt
@@ -1,16 +1,11 @@
 package com.UmayTrade
 
-import android.content.Context
-import com.UmayTrade.extractors.DramaDizilerimExtractor
+import com.lagradost.cloudstream3.plugins.BasePlugin
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
-import com.lagradost.cloudstream3.plugins.Plugin
-
 
 @CloudstreamPlugin
-class DramaDizilerimPlugin: Plugin() {
-    override fun load(context: Context) {
+class DramaDizilerimPlugin : BasePlugin() {
+    override fun load() {
         registerMainAPI(DramaDizilerim())
-
-        registerExtractorAPI(DramaDizilerimExtractor())
     }
 }
