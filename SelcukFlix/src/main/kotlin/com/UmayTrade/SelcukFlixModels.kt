0630c34b5d75df8298d5d3798f7cde522a609857
UmayTrade <kocatepe828@gmail.com>
2026-09-21 03:00:42 +0300

    Refactor data classes for improved structure

--- SelcukFlix/src/main/kotlin/com/UmayTrade/SelcukFlixModels.kt
+++ SelcukFlix/src/main/kotlin/com/UmayTrade/SelcukFlixModels.kt
@@ -1,125 +1,100 @@
-// ! Bu araç @keyiflerolsun tarafından | @KekikAkademi için yazılmıştır.
-
 package com.UmayTrade
 
 import com.fasterxml.jackson.annotation.JsonProperty
 
-data class SearchResult(
-    @JsonProperty("response") val response: String?
-)
-
 data class SearchData(
-    @JsonProperty("state")   val state: Boolean?           = null,
-    @JsonProperty("result")  val result: List<SearchItem>? = arrayListOf(),
-    @JsonProperty("message") val message: String?          = null,
-    @JsonProperty("html")    val html: String?             = null
+    @JsonProperty("state")  val state  : Boolean?         = null,
+    @JsonProperty("result") val result : List<SearchItem> = emptyList(),
+    @JsonProperty("message") val message : String?        = null,
+    @JsonProperty("html")   val html   : String?          = null
 )
 
 data class SearchItem(
-    @JsonProperty("used_slug")         val slug: String?   = null,
-    @JsonProperty("object_name")       val title: String?  = null,
-    @JsonProperty("object_poster_url") val poster: String? = null,
-    @JsonProperty("used_type")         val type: String? = null
+    @JsonProperty("used_slug")        val slug   : String? = null,
+    @JsonProperty("object_name")      val title  : String? = null,
+    @JsonProperty("object_poster_url") val poster : String? = null,
+    @JsonProperty("imdb_point")       val puan   : String? = null
 )
 
-data class Root(
-    @JsonProperty("contentItem") val contentItem: ContentItem,
-    @JsonProperty("RelatedResults") val relatedResults: RelatedResults
+data class ContentDetails(
+    @JsonProperty("contentItem")    val contentItem : MediaItem,
+    @JsonProperty("RelatedResults") val relatedData : RelatedData
 )
 
-data class ContentItem(
-    @JsonProperty("id") val id: Int?,
-    @JsonProperty("original_title") val originalTitle: String?,
-    @JsonProperty("culture_title") val cultureTitle: String?,
-    @JsonProperty("release_year") val releaseYear: Int?,
-    @JsonProperty("total_minutes") val totalMinutes: Int?,
-    @JsonProperty("poster_url") val posterUrl: String?,
-    @JsonProperty("description") val description: String?,
-    @JsonProperty("categories") val categories: String?,
-    @JsonProperty("used_slug") val usedSlug: String?,
-    @JsonProperty("imdb_point") val imdbPoint: Double?
+data class MediaItem(
+    @JsonProperty("original_title") val originalTitle : String? = null,
+    @JsonProperty("release_year")   val releaseYear   : Int?    = null,
+    @JsonProperty("total_minutes")  val totalMinutes  : Int?    = null,
+    @JsonProperty("poster_url")     val posterUrl     : String? = null,
+    @JsonProperty("description")    val description   : String? = null,
+    @JsonProperty("categories")     val categories    : String? = null,
+    @JsonProperty("used_slug")      val usedSlug      : String? = null,
+    @JsonProperty("imdb_point")     val imdbPoint     : Double? = null
 )
 
-data class RelatedResults(
-    @JsonProperty("getContentTrailers") val getContentTrailers: ContentTrailers?,
-    @JsonProperty("getMovieCastsById") val getMovieCastsById: MovieCast?,
-    @JsonProperty("getMoviePartsById") val getMoviePartsById: MovieParts?,
-    @JsonProperty("getMoviePartSourcesById_") val getMoviePartSourcesById: MoviePartsSource?,
-    @JsonProperty("getSerieSeasonAndEpisodes") val getSerieSeasonAndEpisodes: Seasons?,
-    @JsonProperty("getEpisodeSources") val getEpisodeSources: EpisodeSources?,
+data class RelatedData(
+    @JsonProperty("getContentTrailers")       val trailers       : TrailerData?     = null,
+    @JsonProperty("getMovieCastsById")        val cast           : CastData?        = null,
+    @JsonProperty("getMoviePartsById")        val movieParts     : MoviePartsData?  = null,
+    @JsonProperty("getSerieSeasonAndEpisodes") val seriesData    : SeriesData?      = null,
+    @JsonProperty("getEpisodeSources")        val episodeSources : SourcesData?     = null
 )
 
-data class ContentTrailers(
-    @JsonProperty("state") val state: Boolean?,
-    @JsonProperty("result") val result: List<TrailerResult>?
+data class SeriesData(
+    @JsonProperty("result") val seasons : List<SeasonItem>? = null
 )
 
-data class TrailerResult(
-    @JsonProperty("raw_url") val rawUrl: String?,
-    @JsonProperty("raw") val raw: String?
+data class SeasonItem(
+    @JsonProperty("season_no") val seasonNo : Int?             = null,
+    @JsonProperty("episodes")  val episodes : List<EpisodeItem>? = null
 )
 
-data class MovieCast(
-    @JsonProperty("state") val state: Boolean?,
-    @JsonProperty("result") val result: List<MovieResult>?
+data class EpisodeItem(
+    @JsonProperty("episode_no")   val episodeNo : Int?    = null,
+    @JsonProperty("episode_text") val epText    : String? = null,
+    @JsonProperty("used_slug")    val usedSlug  : String? = null
 )
 
-data class MovieResult(
-    @JsonProperty("name") val name: String?,
-    @JsonProperty("cast_image") val castImage: String?
-)
-
-data class MovieParts(
-    @JsonProperty("state") val state: Boolean?,
-    @JsonProperty("result") val result: List<MoviePartsResult>?
-)
-
-data class MoviePartsResult(
-    @JsonProperty("id") val id: Int?,
-)
-
-data class MoviePartsSource(
-    @JsonProperty("state") val state: Boolean?,
-    @JsonProperty("result") val result: List<MoviePartsSourceResult>?
-)
-
-data class MoviePartsSourceResult(
-    @JsonProperty("source_content") val sourceContent: String?,
-    @JsonProperty("quality_name") val qualityName: String?,
+data class SourcesData(
+    @JsonProperty("state")  val state  : Boolean?         = null,
+    @JsonProperty("result") val result : List<SourceItem>? = null
 )
 
 data class SourceItem(
-    val sourceContent: String,
-    val quality: String
+    @JsonProperty("source_content") val sourceContent : String? = null,
+    @JsonProperty("quality_name")   val qualityName   : String? = null
 )
 
-data class ListItems(
-    @JsonProperty("result") val result: List<ContentItem>
+data class VideoSource(
+    val sourceContent : String,
+    val quality       : String
 )
 
-data class Episode(
-    @JsonProperty("season_no") val seasonNo: Int?,
-    @JsonProperty("episode_no") val episodeNo: Int?,
-    @JsonProperty("episode_text") val epText: String?,
-    @JsonProperty("used_slug") val usedSlug: String?,
+data class TrailerData(
+    @JsonProperty("result") val result : List<Trailer>? = null
 )
 
-data class Season(
-    @JsonProperty("season_no") val seasonNo: Int?,
-    @JsonProperty("season_text") val seText: String?,
-    @JsonProperty("used_slug") val usedSlug: String?,
-    @JsonProperty("episodes") val episodes: List<Episode>?,
+data class Trailer(
+    @JsonProperty("trailer_url") val trailerUrl : String? = null
 )
 
-data class Seasons(
-    @JsonProperty("result") val seasons: List<Season>?
+data class CastData(
+    @JsonProperty("result") val result : List<CastMember>? = null
 )
 
-data class EpisodeSources(
-    @JsonProperty("state") val state: Boolean?,
-    @JsonProperty("result") val result: List<MoviePartsSourceResult>?
+data class CastMember(
+    @JsonProperty("actor_name") val actorName : String? = null
 )
 
+data class MoviePartsData(
+    @JsonProperty("result") val result : List<MoviePart>? = null
+)
 
+data class MoviePart(
+    @JsonProperty("original_title") val originalTitle : String? = null,
+    @JsonProperty("used_slug")      val usedSlug      : String? = null
+)
 
-
+data class ApiResponse(
+    @JsonProperty("response") val response : String? = null
+)
