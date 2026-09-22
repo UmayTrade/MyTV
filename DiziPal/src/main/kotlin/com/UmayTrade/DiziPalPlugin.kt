6875a438819acc16e849a6c32e6e95b0a77c9e03
UmayTrade <kocatepe828@gmail.com>
2026-09-23 02:56:11 +0300

    Fix main API registration in DiziPalPlugin
    Updated the main API registration to use the correct class name 'DiziPal'.

--- DiziPal/src/main/kotlin/com/UmayTrade/DiziPalPlugin.kt
+++ DiziPal/src/main/kotlin/com/UmayTrade/DiziPalPlugin.kt
@@ -3,10 +3,12 @@ package com.UmayTrade
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
 import android.content.Context
+import com.keyiflerolsun.DiziPal
 
 @CloudstreamPlugin
 class DiziPalPlugin: Plugin() {
     override fun load(context: Context) {
-        registerMainAPI(DiziPalOriginal())
+        // 'DiziPalOriginal' yerine doğru ana sınıf ismi 'DiziPal' çağrılıyor
+        registerMainAPI(DiziPal())
     }
 }
