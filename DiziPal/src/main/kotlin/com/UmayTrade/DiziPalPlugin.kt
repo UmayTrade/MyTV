f2c92b75f222016caac0cbded82b2423bafc0cdc
UmayTrade <kocatepe828@gmail.com>
2026-09-23 03:27:34 +0300

    Update DiziPalPlugin.kt

--- DiziPal/src/main/kotlin/com/UmayTrade/DiziPalPlugin.kt
+++ DiziPal/src/main/kotlin/com/UmayTrade/DiziPalPlugin.kt
@@ -1,14 +1,12 @@
-package com.UmayTrade
+package com.Blockades
 
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
 import android.content.Context
-import com.keyiflerolsun.DiziPal
 
 @CloudstreamPlugin
 class DiziPalPlugin: Plugin() {
     override fun load(context: Context) {
-        // 'DiziPalOriginal' yerine doğru ana sınıf ismi 'DiziPal' çağrılıyor
         registerMainAPI(DiziPal())
     }
 }
