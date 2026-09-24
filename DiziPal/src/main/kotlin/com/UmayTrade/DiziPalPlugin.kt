9df81bac5b31576d2a411bd5cad799446bd24df6
UmayTrade <kocatepe828@gmail.com>
2026-09-25 01:35:05 +0300

    Update DiziPalPlugin.kt

--- DiziPal/src/main/kotlin/com/UmayTrade/DiziPalPlugin.kt
+++ DiziPal/src/main/kotlin/com/UmayTrade/DiziPalPlugin.kt
@@ -3,6 +3,7 @@ package com.Blockades
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
 import android.content.Context
+import com.UmayTrade.DiziPal
 
 @CloudstreamPlugin
 class DiziPalPlugin: Plugin() {
